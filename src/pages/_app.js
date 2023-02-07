3ada4324c7dc55f97d753e9d8321c6335dcf029a
dayumsam <dayumsam22@gmail.com>
2023-02-07 00:15:47 -0500

    Initial Commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
+import 'react-tooltip/dist/react-tooltip.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div class="container mx-auto">
+      <Component {...pageProps} />
+    </div>
+  )
 }
