940e0044dc364cfa5eea333e31d1405d61967f28
dayumsam <dayumsam22@gmail.com>
2023-02-07 00:19:32 -0500

    Changed a to link tags

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,20 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Navbar() {
   return (
     <nav className="relative w-full flex flex-wrap items-center justify-between py-4 bg-gray-100 text-gray-500 hover:text-gray-700 focus:text-gray-700">
   <div className="container-fluid w-full flex flex-wrap items-center justify-between">
     <div className="container-fluid">
-      <a className="flex items-center text-gray-900 hover:text-gray-900 focus:text-gray-900 mt-2 lg:mt-0 mr-1" href="/">
+    <Link className="flex items-center text-gray-900 hover:text-gray-900 focus:text-gray-900 mt-2 lg:mt-0 mr-1" href="/">
         <Image src="/logo2.png" alt="" width='64' height='64' loading="lazy" />
         <span className="text-xl font-bold pl-3">How They See Us</span>
-      </a>
+    </Link>
     </div>
 
     <div>
-        <a href="#" className="text-lg pl-8 font-bold pl-3">About</a>
-        <a href="#" className="text-lg pl-8 font-bold pl-3">Contact</a>
+        <Link href="#" className="text-lg pl-8 font-bold pl-3">About</Link>
+        <Link href="#" className="text-lg pl-8 font-bold pl-3">Contact</Link>
     </div>
   </div>
 </nav>
